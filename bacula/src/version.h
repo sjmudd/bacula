cfa2c83ef0fa7e12a8fa3d1df058e37046ce0b1d
Kern Sibbald <kern@sibbald.com>
2020-09-20 17:08:16 +0200

    Update version date + ReleaseNotes + ChangeLog

--- bacula/src/version.h
+++ bacula/src/version.h
@@ -6,8 +6,8 @@
 
 /* Note: there can be only *one* VERSION in this file */
 #define VERSION "9.6.6"
-#define BDATE   "30 August 2020"
-#define LSMDATE "30Aug20"
+#define BDATE   "20 September 2020"
+#define LSMDATE "20Sep20"
 
 #define RELEASE 1   /* Use ONLY in rpms */
 
